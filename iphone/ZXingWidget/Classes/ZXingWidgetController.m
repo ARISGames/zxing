18c99aae44c9b7017198108170b97542ee8e9f05
Phildo <Phildo211@gmail.com>
2013-06-26 17:12:41 -0500

    hardcoded forcing of portrait until bug fix from zxing crew

--- iphone/ZXingWidget/Classes/ZXingWidgetController.m
+++ iphone/ZXingWidget/Classes/ZXingWidgetController.m
@@ -552,4 +552,24 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
   return NO;
 }
 
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (BOOL) shouldAutorotate
+{
+    return YES;
+}
+
+- (NSInteger) supportedInterfaceOrientations
+{
+    NSInteger mask = 0;
+    if([self shouldAutorotateToInterfaceOrientation: UIInterfaceOrientationLandscapeLeft])      mask |= UIInterfaceOrientationMaskLandscapeLeft;
+    if([self shouldAutorotateToInterfaceOrientation: UIInterfaceOrientationLandscapeRight])     mask |= UIInterfaceOrientationMaskLandscapeRight;
+    if([self shouldAutorotateToInterfaceOrientation: UIInterfaceOrientationPortrait])           mask |= UIInterfaceOrientationMaskPortrait;
+    if([self shouldAutorotateToInterfaceOrientation: UIInterfaceOrientationPortraitUpsideDown]) mask |= UIInterfaceOrientationMaskPortraitUpsideDown;
+    return mask;
+}
+
 @end
